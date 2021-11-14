c15a9717f8e9893a701642159663912dfcf672ee
emilGame <sousi.hino@outlook.jp>
2021-11-14 19:36:52 +0900

    7つ以上のマッピング修正

--- dapperTest_app/Form1.cs
+++ dapperTest_app/Form1.cs
@@ -29,24 +29,41 @@ namespace dapperTest_app
 
         }
 
-        private List<dynamic> dapper_test1()
+        private List<User> dapper_test1()
         {
-            var sql = @"SELECT * FROM user INNER JOIN sex on sex.sexID = user.Sex left JOIN address as a1 on a1.addressID = user.Address1 left JOIN address as a2 on a2.addressID = user.Address2";
+            var sql = @"SELECT * FROM user INNER JOIN sex on sex.sexID = user.Sex 
+                        left JOIN address as a1 on a1.addressID = user.Address1 
+                        left JOIN address as a2 on a2.addressID = user.Address2
+                        left Join item as i1 on i1.item_id = user.item_1
+                        left Join item as i2 on i2.item_id = user.item_2
+                        left Join item as i3 on i3.item_id = user.item_3
+                        left Join item as i4 on i4.item_id = user.item_4
+                        left Join item as i5 on i5.item_id = user.item_5
+                        left Join item as i6 on i6.item_id = user.item_6
+                        left Join item as i7 on i7.item_id = user.item_7
+                        left Join item as i8 on i8.item_id = user.item_8";
             using (var connection = new MySql.Data.MySqlClient.MySqlConnection("Server=localhost;User=root;Database=test;"))
             {
                 connection.Open();
                 var aaa = connection.Query<User>(sql,
-                    new[] { typeof(User), typeof(Sex), typeof(Address), typeof(Address) }, obj =>
+                    new[] { typeof(User), typeof(Sex), typeof(Address), typeof(Address),
+                            typeof(Item),typeof(Item),typeof(Item),typeof(Item),
+                            typeof(Item),typeof(Item),typeof(Item),typeof(Item)}, obj =>
                           {
                               User user = obj[0] as User;
                               Address A1 = obj[1] as Address;
+                              user.item_name_1 = (obj[4] as Item).item_name;
+                              user.item_name_2 = (obj[5] as Item).item_name;
+                              user.item_name_3 = (obj[6] as Item).item_name;
+                              user.item_name_4 = (obj[7] as Item).item_name;
+                              user.item_name_5 = (obj[8] as Item).item_name;
+                              user.item_name_6 = (obj[9] as Item).item_name;
+                              user.item_name_7 = (obj[10] as Item).item_name;
+                              user.item_name_8 = (obj[11] as Item).item_name;
 
                               return user;
-                          }, splitOn: "UserId,sexID,addressID,addressID").ToList();
-                var bbb = connection.Query(@"SELECT * FROM user INNER JOIN sex on sex.sexID = user.Sex INNER JOIN address on address.addressID = user.Address").ToList();
-
-                var ccc = (User)bbb[0];
-                return bbb;
+                          }, splitOn: "UserId,sexID,addressID,addressID,item_id,item_id,item_id,item_id,item_id,item_id,item_id,item_id").ToList();
+                return aaa;
             }
         }
     }
--- dapperTest_app/User.cs
+++ dapperTest_app/User.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KeyAttribute = Dapper.Contrib.Extensions.KeyAttribute;
+using TableAttribute = Dapper.Contrib.Extensions.TableAttribute;
 
 namespace dapperTest_app
 {
-    [Table("emps")]
+    [Table("user")]
     public class User
     {
         [Key]
@@ -19,18 +22,51 @@ namespace dapperTest_app
         public Sex sex { set; get; }
         public Address a1 { set; get; }
         public Address a2 { set; get; }
-    }
+        public int item_1 { set; get; }
+        public int item_2 { set; get; }
+        public int item_3 { set; get; }
+        public int item_4 { set; get; }
+        public int item_5 { set; get; }
+        public int item_6 { set; get; }
+        public int item_7 { set; get; }
+        public int item_8 { set; get; }
 
+        [Write(false)]
+        public string item_name_1 { set; get; }
+        [Write(false)]
+        public string item_name_2 { set; get; }
+        [Write(false)]
+        public string item_name_3 { set; get; }
+        [Write(false)]
+        public string item_name_4 { set; get; }
+        [Write(false)]
+        public string item_name_5 { set; get; }
+        [Write(false)]
+        public string item_name_6 { set; get; }
+        [Write(false)]
+        public string item_name_7 { set; get; }
+        [Write(false)]
+        public string item_name_8 { set; get; }
+    }
+    [Table("sex")]
     public class Sex
     {
         [Key]
         public int sexID { set; get; }
         public string SexResult { set; get; }
     }
+    [Table("address")]
     public class Address
     {
         [Key]
         public int addressID { set; get; }
         public string addresName { set; get; }
+    }
+    [Table("item")]
+    public class Item
+    {
+        [Key]
+        public int item_id { set; get; }
+        public string item_name { set; get; }
     }
 }
